0b3f2c34b7f9bf276036df136a97bbec5d3552a3
grsagor <grsagor08@gmail.com>
2022-11-30 23:33:03 +0600

    final commit

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../context/AuthProvider';
 
 const MyOrders = () => {
+    const { user } = useContext(AuthContext);
+    const { data: products = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch(`https://assignment-12-server-grsagor.vercel.app/bookings?email=${user?.email}`);
+            const data = await res.json();
+            return data;
+        }
+    });
     return (
         <div>
-            My Orders
+            <h2 className='text-3xl'>My Products</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Book Name</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products?.map(product => <tr className="hover">
+                                <th>{product.bookName}</th>
+                                <td>{product.price}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
